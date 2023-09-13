e06eaee7f13b6578cd80ea08a320224d21064689
MrTahirHabib <54885060+MrTahirHabib@users.noreply.github.com>
2023-09-13 15:32:04 +0500

    Update allpfnositesjavascript.js

--- allpfnositesjavascript.js
+++ allpfnositesjavascript.js
@@ -1,5 +1,19 @@
-//pop up small book download
-    var time = 1000;
-    var welcomeSession = sessionStorage.getItem('welcomeSession');if(welcomeSession === null){$(window).bind('load',function(){setTimeout(function(){$('.trsmallpopbookwrap').css("display","block");welcomeSession = sessionStorage.setItem('welcomeSession',true)}
-    ,time);$('.trsmallpopbookcloseIcon').click(function(){$('.trsmallpopbookwrap').addClass('hide')}
-    )})}
+ // JavaScript code for displaying the popup
+        const popupOverlay = document.querySelector(".popup-overlay");
+        const closePopupBtn = document.querySelector(".close-popup");
+
+        // Check if the popup has been closed in this session
+        const popupClosedSession = sessionStorage.getItem("popupClosedSession");
+
+        // Display the popup after a delay (5 seconds in this example)
+        if (!popupClosedSession) {
+            setTimeout(function() {
+                popupOverlay.style.display = "flex";
+            }, 5000);
+        }
+
+        // Close the popup and set the session storage item
+        closePopupBtn.addEventListener("click", function() {
+            popupOverlay.style.display = "none";
+            sessionStorage.setItem("popupClosedSession", "true");
+        });
